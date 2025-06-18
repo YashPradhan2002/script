30430b4988f5c8ba8613301e2836f77a27ce11f1
Yash Pradhan <yashsmac@Mac.lan>
2025-06-18 17:07:11 +0530

    semi-final

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
 
 const boardRoutes = require('./routes/boardRoute');
 const taskRoutes = require('./routes/taskRoute');
